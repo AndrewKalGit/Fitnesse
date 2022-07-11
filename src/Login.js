fa0f909401ad9b79ae95b7e1da955a472ae50112
Andrew Kal <andrewkalgit@gmail.com>
2022-07-10 20:56:10 -0400

    login page html set up

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 function Login(props) {
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+    }
+
     return (
         <div>
-            <h1>2</h1>
+            <h1>Fitnesse</h1>
+            <form action="userauth" method="post"></form>
+                <div className='img'>
+                    <h1>Login Content Img Here</h1>
+                </div>
+            <div className='container'>
+                <label htmlFor="uname"><b>Username</b></label>
+                <input type="text" placeholder='Enter username' name='uname' required></input>
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="text" placeholder='Enter password' name='psw' required></input>
+                <button type="submit">Login</button>
+                <label>
+                    <p>Remember me</p>
+                    <input type="checkbox" checked={checked} onChange={handleChange}></input> 
+                </label>
+            </div>
         </div>
     );
 }
